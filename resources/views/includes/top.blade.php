951998102525d411d10af4876e04be9012df211c
Taha <ahmed.taha@robovics.com>
2017-11-01 11:10:55 +0200

    social media icons

--- resources/views/includes/top.blade.php
+++ resources/views/includes/top.blade.php
@@ -1,7 +1,7 @@
 <div class="social_media social-list clearfix full-width">
-    <a href="#" target="_blank"><img src="{{ asset('images/facebook.png') }}" alt="Facebook"></a>
-    <a href="#" target="_blank"><img src="{{ asset('images/linkedin.png') }}" alt="LinkedIn"></a>
-    <a href="#" target="_blank"><img src="{{ asset('images/instagram.png') }}" alt="Instagram"></a>
-    <a href="#" target="_blank"><img src="{{ asset('images/youtube.png') }}" alt="YouTube"></a>
-    <a href="#" target="_blank"><img src="{{ asset('images/twitter.png') }}" alt="Twitter"></a>
+    <a href="#" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+    <a href="#" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+    <a href="#" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    <a href="#" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
+    <a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 </div>
