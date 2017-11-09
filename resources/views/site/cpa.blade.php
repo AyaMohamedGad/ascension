7ebead65e3af85d55a24cdd5d528d9c47e0b1c86
ahmed taha <ahmed taha>
2017-11-10 00:50:00 +0200

    'bookinitials'

--- resources/views/site/cpa.blade.php
+++ resources/views/site/cpa.blade.php
@@ -7,7 +7,7 @@
         <div class="header_normal_cover">
             <h2 class="header">{{ trans('locale.cpa_program') }}</h2>
         </div>
-        <div class="tab-content">
+        <div class="tab-content row">
             <div class="col-sm-12">
                 <img class="center" src="{{ asset('images/cpa_logo.jpg') }}" >
             </div>
@@ -27,6 +27,45 @@
                 {{--</div>--}}
             {{--</div>--}}
         </div>
+
+        <div class="row" id="book_tabs">
+            <div class="col-xs-8 col-xs-offset-2">
+                <div class="tabs-left">
+                    <ul class="nav nav-tabs-left">
+                      <li class="active"><a href="#a" data-toggle="tab"><span class="glyphicon glyphicon-heart"></span></a></li>
+                      <li><a href="#b" data-toggle="tab"><span class="glyphicon glyphicon-star"></span></a></li>
+                      <li><a href="#c" data-toggle="tab"><span class="glyphicon glyphicon-headphones"></span></a></li>
+                      <li><a href="#d" data-toggle="tab"><span class="glyphicon glyphicon-time"></span></a></li>
+                    </ul>
+
+                    <ul class="nav nav-tabs-right">
+                      <li><a href="#e" data-toggle="tab"><span class="glyphicon glyphicon-calendar"></span></a></li>
+                      <li><a href="#f" data-toggle="tab"><span class="glyphicon glyphicon-cog"></span></a></li>
+                    </ul>
+
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="a">
+                            <h3>Who do you Love?</h3>
+                        </div>
+                        <div class="tab-pane" id="b">
+                            <h3>What's your Favorite?</h3>
+                        </div>
+                        <div class="tab-pane" id="c">
+                            <h3>What's your Favoritec?</h3>
+                        </div>
+                        <div class="tab-pane" id="d">
+                            <h3>What's your Favorited?</h3>
+                        </div>
+                        <div class="tab-pane" id="e">
+                            <h3>What's your Favoritee?</h3>
+                        </div>
+                        <div class="tab-pane" id="f">
+                            <h3>What's your Favoritef?</h3>
+                        </div>
+                    </div><!-- /tab-content -->
+              </div><!-- /tabbable -->
+            </div><!-- /col -->
+          </div><!-- /row -->
     </div>
     {{--<div class="container clearfix full-width">--}}
         {{--<h2 class="header">6 {{ trans('locale.steps_to_become_cpa') }}</h2>--}}
