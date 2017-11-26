92e6329e94ab18ea9b33d871eecd7818aabe23ef
ahmed taha <ahmed taha>
2017-11-26 22:12:38 +0200

    waiting good book bg

--- resources/views/site/careers.blade.php
+++ resources/views/site/careers.blade.php
@@ -6,10 +6,12 @@
     <div class="container clearfix" data-ref="mixitup-container">
         <div class="header_normal_cover wow fadeInLeft">
             <h2 class="header">{{ trans('locale.careers') }}</h2>
-            <img class="arrow_one wow rotateInDownRight" data-wow-delay=".2s" src="{{ asset('images/arrow_wht.png') }}">
-            <img class="arrow_two wow rotateInUpLeft" data-wow-delay=".3s" src="{{ asset('images/arrow_wht.png') }}">
-            <img class="arrow_three wow rotateInUpRight" data-wow-delay=".4s" src="{{ asset('images/arrow_wht.png') }}">
-            <img class="arrow_four wow rotateInDownLeft" data-wow-delay=".5s" src="{{ asset('images/arrow_wht.png') }}">
+            <div class="arrows_header">
+                <img class="arrow_one wow rotateInDownRight" data-wow-delay=".2s" src="{{ asset('images/arrow_wht.png') }}">
+                <img class="arrow_two wow rotateInUpLeft" data-wow-delay=".3s" src="{{ asset('images/arrow_wht.png') }}">
+                <img class="arrow_three wow rotateInUpRight" data-wow-delay=".4s" src="{{ asset('images/arrow_wht.png') }}">
+                <img class="arrow_four wow rotateInDownLeft" data-wow-delay=".5s" src="{{ asset('images/arrow_wht.png') }}">
+            </div>
         </div>
         <div class="tab-content">
             <div class="col-sm-12 fixing wow bounceInUp">
