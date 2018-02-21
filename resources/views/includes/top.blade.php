577d90f15961e40c9f7cdc9792183f5a5c2e4e68
oyamohamed <aya_m_fcis@yahoo.com>
2018-02-21 22:50:53 +0200

    editings

--- resources/views/includes/top.blade.php
+++ resources/views/includes/top.blade.php
@@ -3,7 +3,7 @@
 	<div class="right-align">
 		<a href="https://www.facebook.com/Ascension-Consulting-and-Training-143527592978917/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
 	    <a href="https://www.linkedin.com/company/ascension_ct/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
-	    <a href="https://www.instagram.com/ascension.ct/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+	    <a href="https://www.instagram.com/ascension_ct/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
 	    <a href="https://www.youtube.com/channel/UClAICI0h0twpKmYusAYzq3A/featured?view_as=subscriber" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
 	    <a href="https://twitter.com/ascension_CT" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 	</div>
